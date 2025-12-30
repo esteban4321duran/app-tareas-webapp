1cb24a2bebda3801acbae73ec1f234ac96e3578b
esteban4321duran <esteban321duran@gmail.com>
2025-12-30 18:37:14 -0300

    set up conneection from drizzle to postgres

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,42 @@
-import { Hono } from 'hono'
-import { renderer } from './renderer'
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Hono } from 'hono';
+import { renderer } from './renderer';
 
-const app = new Hono()
+// Global variable to cache the DB connection across requests on the same worker
+// TODO research more about this Cloudflare Workers feature
+let dbConnection: ReturnType<typeof drizzle> | null = null;
 
-app.use(renderer)
+/* 
+Es posible pasar un parámetro de tipo al método constructor de una app Hono (https://www.typescriptlang.org/docs/handbook/2/generics.html) para especificar los tipos de:
+    * Variables de entorno: Cloudflare worker's Bindings https://hono.dev/docs/getting-started/cloudflare-workers#bindings
+    * Variables arbitrarias enviadas entre middlewares con ciclo de vida limitado a la petición web actual (set - get methods https://hono.dev/docs/api/context#set-get)
+
+https://hono.dev/docs/api/hono#generics
+*/
+interface Singletons {
+    //patrón singleton https://refactoring.guru/es/design-patterns/singleton
+    dbConnection: ReturnType<typeof drizzle>;
+};
+interface EnvironementVariables {
+    DATABASE_URL: string;
+}
+const app = new Hono<{ Bindings: EnvironementVariables, Variables: Singletons }>();
+
+//TODO refactor initDBConnection middleware into persistence layer
+app.use(async (c, next) => {
+    if (!dbConnection) {
+        dbConnection = drizzle(c.env.DATABASE_URL);
+    }
+    //set dbConnection instance so it's available to other middleware
+    c.set('dbConnection', dbConnection);
+
+    await next();
+})
+app.use(renderer);
 
 app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+    const nombre = "florcita";
+    return c.render(<h1>Hello! {nombre}</h1>);
 })
 
-export default app
+export default app;
